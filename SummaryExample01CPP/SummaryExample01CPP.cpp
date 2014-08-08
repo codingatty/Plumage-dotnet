964929ede7039cdc59aeef6a39511c1a0c71e229
Terry Carroll <carroll@tjc.com>
2014-08-07 23:48:13 -0700

    C++ example, retry

---
+++ SummaryExample01CPP/SummaryExample01CPP.cpp
@@ -0,0 +1,31 @@
+// SummaryExample01CPP.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+using namespace System::Collections;
+using namespace System::Collections::Generic;
+
+
+int main(array<System::String ^> ^args)
+{
+	Console::WriteLine("In Summary example c++");
+	Plumage::TSDRReq ^ t =  gcnew Plumage::TSDRReq;
+	t->getTSDRInfo("2564831", "r");  // get info on reg. no 2,564,831
+	Console::WriteLine(t->CSVData);
+	if (t->TSDRMapIsValid){
+		Console::WriteLine("Application serial no: " + t->TSDRMap["ApplicationNumber"]);
+		Console::WriteLine("Trademark text: " + t->TSDRMap["MarkVerbalElementText"]);
+		Console::WriteLine("Application filing date: " + t->TSDRMap["ApplicationDate"]);
+		Console::WriteLine("Registration no: " + t->TSDRMap["RegistrationNumber"]);
+		Console::WriteLine("Status: " + t->TSDRMap["MarkCurrentStatusExternalDescriptionText"]);
+		// Owner info is in most recent (0th) entry in ApplicantList
+		ArrayList^ applicant_list = (ArrayList^)(t->TSDRMap["ApplicantList"]);
+		Dictionary<String ^, Object ^> ^ current_owner_info = (Dictionary<String ^, Object ^> ^)applicant_list[0];
+		Console::WriteLine("Owner: " + current_owner_info["ApplicantName"]);
+		Console::WriteLine("Owner address: " + current_owner_info["ApplicantCombinedAddress"]);
+	};
+
+
+    return 0;
+}
