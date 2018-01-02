009a343d8119357e8412a745d75320c7467860e7
Terry Carroll <carroll@tjc.com>
2018-01-02 07:16:38 -0800

    Using NUnit now

---
+++ Plumage.Tests/UnitTestOffline.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Plumage.Tests
+{
+    [TestFixture]
+    public class UnitTestOffline
+    {
+        [Test]
+        public void Test_A000_null()
+        {
+            TSDRReq t = new TSDRReq();
+            Assert.False(t.XMLDataIsValid);
+            Assert.False(t.CSVDataIsValid);
+            Assert.False(t.TSDRData.TSDRMapIsValid);
+            string cd = System.IO.Directory.GetCurrentDirectory();
+            Console.WriteLine("In Test_A000_null");
+            Console.WriteLine(cd);
+            string a = TestContext.CurrentContext.TestDirectory;
+            Console.WriteLine(a);
+        }
+    }
+}
